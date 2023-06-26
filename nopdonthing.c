898ae6a36a38f406350f636c70ee0bb83b09f922
hundumaaguutamaa <hundumaa.guutamaa@gmail.com>
2023-06-25 23:28:11 -0700

    nopdonthing.c

---
+++ nopdonthing.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+/**
+  *n_nop- Do nothing.
+  *@head: stack head.
+  *@counter: line number. 
+  *
+  *Return: No return. 
+ */
+void n_nop(stack_t **head, unsigned int counter)
+{
+	(void) counter;
+	(void) head;
+}
