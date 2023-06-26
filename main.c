cb8726d9e06805bc56c651c0c2609427285564ea
hundumaaguutamaa <hundumaa.guutamaa@gmail.com>
2023-06-25 23:25:47 -0700

    main.c

---
+++ main.c
@@ -0,0 +1,51 @@
+#include "monty.h"
+global_var gvar = {NULL, NULL, NULL, 0};
+
+/**
+* main - Monty main
+* @argc: Number of arguments in main. 
+* @argv: Argument vector. 
+* Return: 0 (success)
+*/
+int main(int argc, char *argv[])
+{
+	char *content;
+	FILE *file;
+
+	size_t size = 0;
+	ssize_t read_line = 1;
+	stack_t *stack = NULL;
+	
+	unsigned int counter = 0;
+
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+	file = fopen(argv[1], "r");
+	gvar.file = file;
+
+	if (!file)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+	while (read_line > 0)
+	{
+		content = NULL;
+		read_line = getline(&content, &size, file);
+		gvar.content = content;
+		counter++;
+		
+		if (read_line > 0)
+		{
+			execute(content, &stack, counter, file);
+		}
+		free(content);
+	}
+	free_stack(stack);
+	fclose(file);
+
+return (0);
+}
