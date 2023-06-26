291ca65323204fd26c199f0f03b740125cc26d6e
hundumaaguutamaa <hundumaa.guutamaa@gmail.com>
2023-06-25 23:32:17 -0700

    stack

---
+++ stack.c
@@ -0,0 +1,15 @@
+#include "monty.h"
+/**
+ * st_stack - prints the top stack vlaues.
+ * @head: Stack head.
+ * @counter: Line number.
+ *
+ * Return: Nothing
+*/
+void st_stack(stack_t **head, unsigned int counter)
+{
+	(void)head;
+	(void)counter;
+
+	gvar.lifi = 0;
+}
